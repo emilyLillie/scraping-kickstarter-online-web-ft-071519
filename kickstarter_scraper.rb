126aa19431ca430c969a436eef3fc788663c2ece
Emily Lillie <emilylillie3@gmail.com>
2019-08-01 23:27:59 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,9 @@ require "pry"
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location:
+
 
 def create_project_hash
   # This just opens a file and reads it into a variable
