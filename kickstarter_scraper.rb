288070370cc0223bc80ec701d324ba360b1ae0e7
Emily Lillie <emilylillie3@gmail.com>
2019-08-01 23:24:03 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,6 +4,7 @@ require "pry"
 
 # projects: kickstarter.css("li.project.grid_4")
 
+
 def create_project_hash
   # This just opens a file and reads it into a variable
    html = File.read('fixtures/kickstarter.html')
